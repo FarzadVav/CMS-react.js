081cc444f2ebcc163981fa9e61ab75d4eb89a672
FarzadVav <vav.progg.ir@gmail.com>
2023-05-17 03:57:34 -0700

    change style in addNewProduct

--- frontend/src/Components/AddNewProduct.jsx
+++ frontend/src/Components/AddNewProduct.jsx
@@ -39,7 +39,7 @@ function AddNewProduct() {
             <input type="text"
               className='text-blue-500 placeholder:text-blue-500 border-blue-500 ring-0 border
               w-full px-5 pl-12 py-2.5 rounded-full focus:outline-0 focus:ring-4 focus:ring-blue-200'
-              placeholder='عکس محصول' />
+              placeholder='تعداد رنگ بندی محصول' />
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="stroke-blue-500 absolute left-4 w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
@@ -68,7 +68,7 @@ function AddNewProduct() {
             <input type="text"
               className='text-blue-500 placeholder:text-blue-500 border-blue-500 ring-0 border
               w-full px-5 pl-12 py-2.5 rounded-full focus:outline-0 focus:ring-4 focus:ring-blue-200'
-              placeholder='تعداد رنگ بندی محصول' />
+              placeholder='عکس محصول' />
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="stroke-blue-500 absolute left-4 w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
