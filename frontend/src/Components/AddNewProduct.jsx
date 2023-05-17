7611fbdca9a740c2d2e5a4f0bfbac5dc742879e4
FarzadVav <vav.progg.ir@gmail.com>
2023-05-17 03:49:21 -0700

    changr style in addNewProduct

--- frontend/src/Components/AddNewProduct.jsx
+++ frontend/src/Components/AddNewProduct.jsx
@@ -6,15 +6,6 @@ function AddNewProduct() {
       <h1 className='text-slate-700 w-full text-3xl text-right sansbold'>ثبت محصول جدید</h1>
       <form action="#" className='w-full flex flex-col gap-3'>
         <div className="w-full flex justify-center items-center gap-3">
-        <div className="w-1/3 flex justify-center items-center relative">
-            <input type="text"
-              className='text-blue-500 placeholder:text-blue-500 border-blue-500 ring-0 border
-              w-full px-5 pl-12 py-2.5 rounded-full focus:outline-0 focus:ring-4 focus:ring-blue-200'
-              placeholder='نام محصول' />
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="stroke-blue-500 absolute left-4 w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-          </div>
           <div className="w-1/3 flex justify-center items-center relative">
             <input type="text"
               className='text-blue-500 placeholder:text-blue-500 border-blue-500 ring-0 border
@@ -28,18 +19,27 @@ function AddNewProduct() {
             <input type="text"
               className='text-blue-500 placeholder:text-blue-500 border-blue-500 ring-0 border
               w-full px-5 pl-12 py-2.5 rounded-full focus:outline-0 focus:ring-4 focus:ring-blue-200'
-              placeholder='نام محصول' />
+              placeholder='قیمت محصول' />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="stroke-blue-500 absolute left-4 w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+          </div>
+          <div className="w-1/3 flex justify-center items-center relative">
+            <input type="text"
+              className='text-blue-500 placeholder:text-blue-500 border-blue-500 ring-0 border
+              w-full px-5 pl-12 py-2.5 rounded-full focus:outline-0 focus:ring-4 focus:ring-blue-200'
+              placeholder='موجودی محصول' />
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="stroke-blue-500 absolute left-4 w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
           </div>
         </div>
         <div className="w-full flex justify-center items-center gap-3">
-        <div className="w-1/3 flex justify-center items-center relative">
+          <div className="w-1/3 flex justify-center items-center relative">
             <input type="text"
               className='text-blue-500 placeholder:text-blue-500 border-blue-500 ring-0 border
               w-full px-5 pl-12 py-2.5 rounded-full focus:outline-0 focus:ring-4 focus:ring-blue-200'
-              placeholder='نام محصول' />
+              placeholder='عکس محصول' />
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="stroke-blue-500 absolute left-4 w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
@@ -48,7 +48,7 @@ function AddNewProduct() {
             <input type="text"
               className='text-blue-500 placeholder:text-blue-500 border-blue-500 ring-0 border
               w-full px-5 pl-12 py-2.5 rounded-full focus:outline-0 focus:ring-4 focus:ring-blue-200'
-              placeholder='نام محصول' />
+              placeholder='میزان محبوبیت محصول' />
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="stroke-blue-500 absolute left-4 w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
@@ -57,18 +57,29 @@ function AddNewProduct() {
             <input type="text"
               className='text-blue-500 placeholder:text-blue-500 border-blue-500 ring-0 border
               w-full px-5 pl-12 py-2.5 rounded-full focus:outline-0 focus:ring-4 focus:ring-blue-200'
-              placeholder='نام محصول' />
+              placeholder='میزان فروش محصول' />
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="stroke-blue-500 absolute left-4 w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
           </div>
         </div>
-        <button type='submit' className='btn btn-success w-full'>
-          اضافه کردن
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
-          </svg>
-        </button>
+        <div className="w-full flex justify-center items-center gap-3">
+          <div className="w-1/3 flex justify-center items-center relative">
+            <input type="text"
+              className='text-blue-500 placeholder:text-blue-500 border-blue-500 ring-0 border
+              w-full px-5 pl-12 py-2.5 rounded-full focus:outline-0 focus:ring-4 focus:ring-blue-200'
+              placeholder='تعداد رنگ بندی محصول' />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="stroke-blue-500 absolute left-4 w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+          </div>
+          <button type='submit' className='btn btn-success w-1/3'>
+            اضافه کردن
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
+            </svg>
+          </button>
+        </div>
       </form>
     </div>
   )
