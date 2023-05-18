60a4b91d1e6b0520210520a03a22bd7990adea8b
FarzadVav <vav.progg.ir@gmail.com>
2023-05-18 08:47:25 -0700

    add modal class

--- frontend/src/Components/DeleteModal.jsx
+++ frontend/src/Components/DeleteModal.jsx
@@ -2,9 +2,7 @@ import React, { useState } from 'react'
 
 function DeleteModal({ showModal, closeModalHandler }) {
   return (
-    <div className={`${showModal ? 'flex' : 'hidden'}
-      bg-black bg-opacity-25 backdrop-blur-md w-screen h-screen
-      justify-center items-center fixed top-0 left-0`}>
+    <div className={showModal ? 'bg-modal' : 'hidden'}>
       <div className='bg-white h-48 flex flex-col justify-center items-center gap-6 rounded-2xl p-9'>
         <h1 className='text-2xl text-slate-700'>آیا از حذف کردن اطمینان دارید؟</h1>
         <div className="w-full flex justify-center items-center gap-3">
